c96778c63a9edf63eef8f2b17234a8304a989ccf
Johnathan-EricXdev <jonathanxdev@163.com>
2019-01-02 14:55:44 +0800

    测试重新登录提交
    惊呆了萨金陵饭店

--- dvote001/addons/tyzm_diamondvote/inc/web/download.inc.php
+++ dvote001/addons/tyzm_diamondvote/inc/web/download.inc.php
@@ -44,6 +44,7 @@ foreach ($list as $mid => $value) {
 	//this is push by jonathanxdev
 	//this is push by jonathanxdev too 2019-1-2 14:42:28
 	//this is push by jonathan-Ericxdev too 2019-1-2 14:50:37
+	//this is push by jonathan-Ericxdev SecondTimeSignin 2019-1-2 14:54:33
 	//print_r($status);exit;
 	$html .= $value['id'] . "\t ,";	
 	$html .= $value['noid'] . "\t ,";	
